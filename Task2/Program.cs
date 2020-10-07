cf5382f7635dc9e686accf2c0c13c8ebf29903f0
Opilane TTHK <opilane@tthk.ee>
2020-10-07 15:57:00 +0300

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "1, 2, 3, 4, 5, 6, 7, 8, 9, 10" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, numbers.Length);
+            Console.WriteLine($" This is your random number: {numbers[randomIndex]} ");
+
+
+
+
+        }
+    }
+}
---
+++ Week6Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Week6Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] superHeroes = { "Batman", "Santa", "Spongebob", "Thor", "Harry Potter" };
+            string[] superVillains = { "Godzilla", "Voldemort", "Grinch", "Darth Vader", "T-1000" };
+            string[] weapon = { "banana", "fork", "keyboard", "purse", "jar of mud" };
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, superHeroes.Length);
+            int randomIndexx = rnd.Next(0, superVillains.Length);
+            int randomIndexxx = rnd.Next(0, weapon.Length);
+            Console.WriteLine($"Today {superHeroes[randomIndex]} will fight {superVillains[randomIndexx]} with a {weapon[randomIndexxx]}");
+
+        }
+    }
+}
